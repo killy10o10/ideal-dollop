5da5b732d6a0a7e91eb1bb1bc5495285fb39ff31
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-11-08 19:59:05 +0000

    Get user's current location

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
 /* eslint-disable no-undef */
 import { Spinner } from '@material-tailwind/react';
 import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+import { useEffect, useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const API_KEY = import.meta.env.VITE_GOOGLE_PLACES_KEY;
 
-  const center = { lat: 5.564889, lng: -0.2618898 }
+  const [center, setCenter] = useState({ lat: 6.6696191, lng: -1.6299929 });
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: API_KEY,
   });
 
-  console.log(isLoaded)
+  useEffect(() => {
+    // Check if the Geolocation API is available in the user's browser
+    if ("geolocation" in navigator) {
+      // Get the user's current location
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          // Extract latitude and longitude from the position object
+          const { latitude, longitude } = position.coords
+
+          // Update the center with the user's location
+          setCenter({ lat: latitude, lng: longitude });
+          toast.success("Access granted")
+        },
+        function (error) {
+          // Handle any errors that occur during geolocation retrieval
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.error("User denied the request for Geolocation.");
+              toast.error("User denied the request for Geolocation.")
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.error("Location information is unavailable.");
+              toast.error("Location information is unavailable.")
+              break;
+            case error.TIMEOUT:
+              console.error("The request to get user location timed out.");
+              toast.error("The request to get user location timed out.")
+              break;
+            default:
+              console.error("An unknown error occurred.");
+              toast.error("An unknown error occurred.")
+              break;
+          }
+        }
+      );
+    } else {
+      console.error("Geolocation is not available in this browser.");
+      toast.error("Geolocation is not available in this browser.")
+    }
+  }, []);
 
   if (!isLoaded) {
     return (
       <div className='h-screen flex flex-col items-center justify-center'>
-        <Spinner className='h-16 w-16 text-gray-900/50' />{' '}
+        <Spinner className='h-16 w-16 text-gray-900/50' />
       </div>
     );
   }
@@ -27,8 +68,10 @@ function App() {
 
         </GoogleMap>
       </div>
+      <Toaster />
     </>
   );
 }
 
 export default App;
+
