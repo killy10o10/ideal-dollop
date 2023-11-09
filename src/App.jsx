4273fa52fad14ee52ba8a6738b4b2b20acbb3093
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-11-09 01:14:53 +0000

    Add route generator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
 import { Spinner } from '@material-tailwind/react';
-import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
-import { useEffect, useState } from "react"
+import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import RouteGenerator from './components/RouteGenerator';
+
 
 function App() {
   const API_KEY = import.meta.env.VITE_GOOGLE_PLACES_KEY;
 
-  const [center, setCenter] = useState({ lat: 6.6696191, lng: -1.6299929 });
+  const [center, setCenter] = useState({ lat: 5.551475, lng: -0.194492 });
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: API_KEY,
@@ -15,42 +17,37 @@ function App() {
 
   useEffect(() => {
     // Check if the Geolocation API is available in the user's browser
-    if ("geolocation" in navigator) {
+    if ('geolocation' in navigator) {
       // Get the user's current location
       navigator.geolocation.getCurrentPosition(
         function (position) {
           // Extract latitude and longitude from the position object
-          const { latitude, longitude } = position.coords
+          const { latitude, longitude } = position.coords;
 
           // Update the center with the user's location
           setCenter({ lat: latitude, lng: longitude });
-          toast.success("Access granted")
+          toast.success('Current user location');
         },
         function (error) {
           // Handle any errors that occur during geolocation retrieval
           switch (error.code) {
             case error.PERMISSION_DENIED:
-              console.error("User denied the request for Geolocation.");
-              toast.error("User denied the request for Geolocation.")
+              toast.error('request for Geolocation denied.');
               break;
             case error.POSITION_UNAVAILABLE:
-              console.error("Location information is unavailable.");
-              toast.error("Location information is unavailable.")
+              toast.error('Location information is unavailable.');
               break;
             case error.TIMEOUT:
-              console.error("The request to get user location timed out.");
-              toast.error("The request to get user location timed out.")
+              toast.error('The request to get user location timed out.');
               break;
             default:
-              console.error("An unknown error occurred.");
-              toast.error("An unknown error occurred.")
+              toast.error('An unknown error occurred.');
               break;
           }
         }
       );
     } else {
-      console.error("Geolocation is not available in this browser.");
-      toast.error("Geolocation is not available in this browser.")
+      toast.error('Geolocation is not available in this browser.');
     }
   }, []);
 
@@ -63,9 +60,20 @@ function App() {
   }
   return (
     <>
-      <div className='h-screen'>
-        <GoogleMap center={center} zoom={15} mapContainerStyle={{ width: '100%', height: '100%' }}>
-
+      <div className='h-screen relative flex flex-col'>
+        <RouteGenerator />
+        <GoogleMap
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+        >
+          <Marker position={center} />
         </GoogleMap>
       </div>
       <Toaster />
@@ -74,4 +82,3 @@ function App() {
 }
 
 export default App;
-
---
+++ src/components/RouteGenerator.jsx
@@ -0,0 +1,37 @@
+import { Input } from '@material-tailwind/react';
+import { CiSearch } from 'react-icons/ci';
+import { AiOutlineClose } from 'react-icons/ai';
+import { SlLocationPin } from 'react-icons/sl';
+import { TbLocationFilled } from 'react-icons/tb';
+
+function RouteGenerator() {
+  return (
+    <div className='bg-white self-center justify-self-center absolute top-10 rounded p-4 z-50 flex flex-col gap-6'>
+      <div className='flex flex-col md:flex-row gap-3'>
+        <Input
+          placeholder='Enter Location'
+          icon={<SlLocationPin />}
+        />
+        <Input
+          placeholder='Enter interest'
+          icon={<CiSearch />}
+        />
+        <button className='bg-pink-500 rounded-md hover:bg-pink-600 whitespace-nowrap px-5 text-white font-bold active:scale-95 transition-all'>
+          Generate Route
+        </button>
+        <button className='bg-blue-gray-50 hover:bg-gray-300 rounded-lg px-2 flex items-center active:scale-95 transition-all w-fit'>
+          <AiOutlineClose className='text-2xl' />
+        </button>
+      </div>
+      <div className='flex items-center justify-between'>
+        <p>Distance:</p>
+        <p>Duration:</p>
+        <button className='bg-blue-gray-50 hover:bg-gray-300 rounded-full border-2 border-blue-500 p-2 flex items-center active:scale-95 transition-all'>
+          <TbLocationFilled />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default RouteGenerator;
