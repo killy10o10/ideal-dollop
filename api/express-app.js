53e796fde9393c42fe427f96a70c3a5bad0c1df5
Kwasi <stawuah18@gmail.com>
2023-12-27 13:46:46 +0000

    Auth completed :package:

--- api/express-app.js
+++ api/express-app.js
@@ -5,6 +5,16 @@ const cors = require('cors');
 
 module.exports = async (app) => {
 
+    app.use((err, req, res, next) => {
+        const statusCode = err.statusCode || 500;
+        const message = err.message || 'Internal Server Error';
+        return res.status(statusCode).json({
+            success: false,
+            statusCode,
+            message,
+        });
+    });
+
     app.use(express.json({ limit: '1mb' }));
     app.use(express.urlencoded({ extended: true, limit: '1mb' }));
     app.use(cors());
